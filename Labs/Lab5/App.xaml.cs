c1a854d987f569d99ea7396326c75497feee15dc
AganichevAG <AganichevAG@mer.ci.nsu.ru>
2025-03-29 15:48:26 +0700

    Начал Lab5

---
+++ Labs/Lab5/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab5;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private readonly IServiceProvider _serviceProvider;
+    public App()
+    {
+        var services = new ServiceCollection();
+        services.AddTransient<MainWindow>();
+        _serviceProvider = services.BuildServiceProvider();
+    }
+    protected override void OnStartup(StartupEventArgs e)  //обработчик ивента startUp
+    {
+        var window = new MainWindow();
+        window.Show();
+    }
+}
+
