b7bafa04a0c96b93892b63763941a348a576b154
AganichevAG <AganichevAG@mer.ci.nsu.ru>
2025-04-05 16:24:29 +0700

    самое начало

---
+++ Labs/Lab6/Var1/DataBase/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var1.DataBase
+{
+    class AppDbContext : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=./app.db");
+        }
+
+    }
+}
---
+++ Labs/Lab6/Var1/DataBase/Request.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var1.DataBase
+{
+    class Request
+    {
+        public int ID { get; set; }
+        public int Number { get; set; }
+        public DateTime DateAdd { get; set; }
+        public string TypeCar { get; set; }
+        public string ModelCar { get; set; }
+        public string ProblemDescription {  get; set; }
+        
+        public User UserId { get; set; }
+        public int PhoneNumber { get; set; }
+
+
+    }
+}
